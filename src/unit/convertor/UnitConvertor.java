b89d67ea49ac260702d25f46543c8377744dbf32
salah chenikhar <salah1indz@gmail.com>
2025-07-23 12:07:53 +0100

    Update UnitConvertor.java

--- src/unit/convertor/UnitConvertor.java
+++ src/unit/convertor/UnitConvertor.java
@@ -1,18 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package unit.convertor;
 
-/**
- *
- * @author leila
- */
 public class UnitConvertor {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         GUI gui = new GUI ();
         gui.setVisible(true);
